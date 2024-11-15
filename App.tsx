1f7fb99bb6923964bb7dcca399a4d3bc8f97e019
Rodrigo5431 <rodrigokarvalho12890@gmail.com>
2024-11-14 21:23:29 -0300

    inspiracao

--- App.tsx
+++ App.tsx
@@ -7,8 +7,8 @@ export default function App() {
   return (
     <View>
       <StatusBar style="auto" />
-      {/* <Login/> */}
-      <Home/>
+      <Login/>
+      {/* <Home/> */}
     </View>
   );
 }
